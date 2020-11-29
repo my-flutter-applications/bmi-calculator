49298d2d78e0c632520dd84c6ed0317a91cec2bc
Supriya Malla <supriya.malla02@gmail.com>
2020-11-29 08:44:31 +0545

    bmi-calculator

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.onTap, @required this.buttonTitle});
+  final Function onTap;
+  final String buttonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.only(top: 10.0),
+        color: kBottomContainerColour,
+        height: kBottomContainerHeight,
+        width: double.infinity,
+        padding: EdgeInsets.only(bottom: 20.0),
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
